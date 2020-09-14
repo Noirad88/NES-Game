c36dcc1385c883acb011b64e6d576b168d46ff79
darionm <noirad@gmail.com>
2020-09-14 03:12:06 -0400

    added draw buffer

--- helloworld.asm
+++ helloworld.asm
@@ -1,44 +1,70 @@
-no_attributes =     %00000000
-top_of_screen =     $08
-bottom_of_screen =  $08
-nametable_max =     $383
-direction_up =      $01
-direction_down =    $02
-direction_left =    $03
-direction_right =   $04
-moveAmount =        $08
+no_attributes =         %00000000
+top_of_screen =         $08
+bottom_of_screen =      $08
+nametable_max =         $383
+direction_up =          $01
+direction_down =        $02
+direction_left =        $03
+direction_right =       $04
+moveAmount =            $08
+drawingOperationPerFrame =  $02
+drawBuffer =                $00F1  
 
-;core drawing function; parameters: \1 = metasprite, \2 = low_byte_position, \3 = high_byte_position,
-SET_METASPRITE_VARS MACRO 
-                    ; assigning relative variables
-                    LDY #$00
-                    LDA \1, Y
-                    STA draw_loop_width             ;we tell the operation how wide (in tiles) the metasprite is
-                    LDY #$01
-                    LDA \1, Y
-                    STA draw_loop_height            ;we tell the operation how tall (in tiles) the metasprite is
-                    LDA #$00
-                    STA draw_loop_height_counter    ;this keeps track of our rows so we can check draw_loop_height to end
-                    LDA \2
-                    STA draw_loop_start_low_byte    ;to store/alter low byte of adress
-                    LDA \3
-                    STA draw_loop_start_high_byte   ;to store/alter high byte of adress
-                    LDA #$00
-                    STA draw_loop_current_x         ;this keeps track of our current x positon so we can check draw_loop_width
-                                                    ;to know when to jump to the next row
-                    ;we start to read the metasprite ahead 2 since the first two pos are w,h
-                    LDY #$02
-                    ENDM
+ADD_DRAW_REQUEST_METASPRITE MACRO ; adds a set of ops to drawing buffer for a metasprite (collection of sprites)
+                            PUSH_DRAWING_BUFFER #LOW(\1) ; sprite high byte
+                            PUSH_DRAWING_BUFFER #HIGH(\1) ; sprite low byte
+                            PUSH_DRAWING_BUFFER \3 ; pos high byte
+                            PUSH_DRAWING_BUFFER \4 ; pos low byte
+                            ENDM
 
-CHANGE_BG_TILE MACRO               
-               LDX $2002             ; read PPU status to reset the high/low latch
-               LDX #$20
-               STX $2006             ; write the high db of $2000 address
-               LDX \1                ; low byte var is first
-               STX $2006             ; write the low db of $2000 address
-               LDX \2                ; tile hex
-               STX $2007
-               ENDM
+ADD_DRAW_REQUEST_TILE       MACRO ; adds a set of ops to drawing buffer 
+                            PUSH_DRAWING_BUFFER #$00 ; sprite high byte (tiles never have high bytes)
+                            PUSH_DRAWING_BUFFER \1   ; sprite low byte
+                            PUSH_DRAWING_BUFFER \2   ; pos high byte
+                            PUSH_DRAWING_BUFFER \3   ; pos low byte
+                            ENDM
+
+PUSH_DRAWING_BUFFER    MACRO ; adds op to drawing buffer
+                       lda \1
+                       ldx drawBufferPointer
+                       sta drawBuffer, X
+                       inx
+                       stx drawBufferPointer
+                       ENDM
+
+POP_DRAWING_BUFFER     MACRO ; removes the top item and assigns accumulator
+                       ldx drawBufferPointer
+                       lda #$00
+                       dex
+                       ldy drawBuffer, X
+                       sta drawBuffer, X
+                       stx drawBufferPointer
+                       tya
+                       ENDM
+
+CLEAR_DRAWING_BUFFER   MACRO ; clears drawing buffer
+                       ldx #$00
+                       lda #$00
+                       drawingLoop:
+                       sta drawBuffer, X
+                       inx
+                       cpx #$16
+                       bne drawingLoop
+                       ldx #$00
+                       stx drawBufferPointer
+                       ENDM
+
+;change a single tile (more efficient function if you're not drawing a metasprite) \1=tile hex, \2=low byte, \3=high byte
+SET_BG_TILE MACRO               
+
+            LDX $2002             ; read PPU status to reset the high/low latch
+            LDX draw_loop_start_high_byte
+            STX $2006             ; write the high db of $2000 address
+            LDX draw_loop_start_low_byte                ; low byte var is first
+            STX $2006             ; write the low db of $2000 address
+            LDX draw_loop_sprite                ; tile hex
+            STX $2007
+            ENDM
 
 RESET_SCROLL MACRO
              LDA world_scroll_x
@@ -50,6 +76,11 @@ RESET_SCROLL MACRO
 MACRO_ADD_A MACRO ;adds using CLC  
             CLC
             ADC \1
+            ENDM
+
+MACRO_SUB_A MACRO ;adds using CLC  
+            CLC
+            SBC \1
             ENDM
 
   .inesprg 1   ; 1x 16KB PRG code
@@ -71,7 +102,7 @@ low_byte_count      .rs 1
 do_draw             .rs 1
 world_scroll_x      .rs 1
 world_scroll_y      .rs 1
-nmi_flag            .rs 1
+
 ; variables for core drawing operaions
 draw_loop_start_low_byte  .rs 1
 draw_loop_start_high_byte .rs 1
@@ -79,6 +110,12 @@ draw_loop_width           .rs 1
 draw_loop_height          .rs 1
 draw_loop_height_counter  .rs 1
 draw_loop_current_x       .rs 1
+draw_loop_sprite_low_byte         .rs 1
+draw_loop_sprite_high_byte         .rs 1
+draw_loop_operation       .rs 1
+
+  .rsset $00F0
+drawBufferPointer      .rs 1
 
 ;;;;;;;;;;;;;;;
 
@@ -87,13 +124,15 @@ draw_loop_current_x       .rs 1
 
   .include "background_tiles"
 
+;this is where we buffer drawings to be used in NMI
+
 metasprite:
 
   ;first row is data: w, h,
-  .byte $04,$04
+  .byte $0b,$04
   
   ;the draw operation fetches the width first and initially sets to y 
-	.byte $09,$09,$09,$09
+	.byte $03,$02,$09,$02
   .byte $02,$02,$02,$02
   .byte $09,$09,$09,$09
   .byte $09,$09,$09,$09
@@ -220,27 +259,35 @@ EnablingSpritesAndBackgrounds:
   LDA #$00
   STA $2005
 
-  LDA $0203        ;fetching player variables
+InitiliazingVars:
+  LDA $0200        ;fetching player variables
   STA player_x
   LDA $0200
   STA player_y
-
+  
   LDA #$00          ;setting our test variable
   STA do_draw
   STA world_scroll_x
   STA world_scroll_y
 
 Sounds:
-  
+
+
+;----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
 GameLoop: ;------------------------------------------------------------------------------
   
-vblankwait3:       ; First wait for vblank to make sure PPU is ready
-  BIT $2002
-  BPL vblankwait3
-
-  LDA #$00
-  STA nmi_flag
-
 CheckVelocityBeforeLatchingController:
 
   LDA #$00
@@ -261,7 +308,7 @@ LatchController:
   LDA #$00
   STA $4016 
 
-ReadA:
+  ReadA:
 
   LDA #$00
   STA do_draw
@@ -271,37 +318,39 @@ ReadA:
                   ; add instructions here to do something when button IS pressed (1)
   LDA #$01
   STA do_draw
+  
+  ReadADone:        ; handling this button is done
 
-ReadADone:        ; handling this button is done
-
-ReadB: 
+  ReadB: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadBDone   ; branch to ReadADone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
 
-ReadBDone:        ; handling this button is done
+  ADD_DRAW_REQUEST_METASPRITE metasprite, #HIGH(metasprite), #$20, #$2D
 
-ReadSelect: 
+  ReadBDone:        ; handling this button is done
+
+  ReadSelect: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadSelectDone   ; branch to ReadADone if button is NOT pressed (0)
 
-ReadSelectDone:
+  ReadSelectDone:
 
-ReadStart: 
+  ReadStart: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadStartDone   ; branch to ReadADone if button is NOT pressed (0)
 
-ReadStartDone:
+  ReadStartDone:
 
-ReadUp: 
+  ReadUp: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadDown   ; branch to ReadADone if button is NOT pressed (0)
 
-SetUp: ;set player up
+  SetUp: ;set player up
   LDX #direction_up
   STX player_move_dir
   ;reset our velocity
@@ -309,12 +358,12 @@ SetUp: ;set player up
   STX player_vel
   JMP MovePlayer
 
-ReadDown: 
+  ReadDown: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadLeft   ; branch to ReadADone if button is NOT pressed (0)
 
-SetDown: ;set player down
+  SetDown: ;set player down
   LDX #direction_down
   STX player_move_dir
   ;reset our velocity
@@ -322,12 +371,12 @@ SetDown: ;set player down
   STX player_vel
   JMP MovePlayer
 
-ReadLeft: 
+  ReadLeft: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ ReadRight
   
-SetLeft: ;set player left
+  SetLeft: ;set player left
   LDX #direction_left
   STX player_move_dir
   ;reset our velocity
@@ -335,12 +384,12 @@ SetLeft: ;set player left
   STX player_vel
   JMP MovePlayer 
 
-ReadRight: 
+  ReadRight: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
   BEQ MovePlayer
 
-SetRight: ;set player right
+  SetRight: ;set player right
   LDX #direction_right
   STX player_move_dir
   ;reset our velocity
@@ -354,7 +403,7 @@ MovePlayer:
   BNE MoveUp    ; if player is set to move, continue movement along 8 pixels
   JMP MovePlayerEnd
 
-MoveUp:
+  MoveUp:
   ;if this direction is not set, check next direction
   LDX player_move_dir    
   CPX #direction_up 
@@ -377,7 +426,7 @@ MoveUp:
   STY player_vel   
   JMP Animate 
 
-MoveDown:
+  MoveDown:
   LDX player_move_dir  
   CPX #direction_down 
   BNE MoveLeft            
@@ -397,8 +446,7 @@ MoveDown:
   STY player_vel   
   JMP Animate
 
-MoveLeft:
-
+  MoveLeft:
   LDX player_move_dir    
   CPX #direction_left 
   BNE MoveRight            
@@ -418,8 +466,7 @@ MoveLeft:
   STY player_vel    
   JMP Animate
 
-MoveRight:
-
+  MoveRight:
   LDX player_move_dir   
   CPX #direction_right 
   BNE MovePlayerEnd            
@@ -438,49 +485,138 @@ MoveRight:
   DEY
   STY player_vel  
 
-Animate:
-; we jump to here if we have moved, so this is the place where we animate
+Animate: ; we jump to here if we have moved, so this is the place where we animate
 
 MovePlayerEnd:
+
+
+
+
+
+vblankwait3:       ; First wait for vblank to make sure PPU is ready; keeps game at 60 (50?) fps
+  BIT $2002
+  BPL vblankwait3
+
 
   JMP GameLoop ;---------------------------------------------------------------------
 
 
+
+
+
+
+
+
+
+
 NMI: ;start of our game loop ------------------------------------------------------
-  LDA #$01        ; just test/checking NMI for understanding
-  STA nmi_flag 
+ 
+  pha ;hold our registers incase the NMI changes them
+  tya
+  pha
+  txa
+  pha
 
-  LDA #%10010000  ;refresh PPU
+  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0
   STA $2000
-  LDA #%00011110   
+  LDA #%00011110   ; enable background and sprites
   STA $2001
-  ; done setting up PPU
 
-CheckDrawingBuffer:   ;checking what draw requests have been added, if any
+
+CheckDrawingBuffer:   ;checking what draw requests have been added to the stack, if any
+
+  ;drawing buffer: 01do - 01ef
+  ;draw metasprite: 01, 00, 00, 00
+  ;draw tile:       02, 00, 00, 00
   
-  LDA do_draw
-  CMP #$01
-  BEQ drawLoopSetup
-  JMP EndDrawLogic
+  ;to prevent running out of time, we limit the amount of drawing operations per frame (drawingOperationLimit)
+  ;max, we probably only want to allow 2 drawing operations per frame.
+  ;maybe one if we can get away with it.
+  ;we'll need to fetch the stack pointer (TSX) next frame to continue from where we left off 
+  ;if the current byte the stack pointer sits on is 00 on the next frame, then we have nothing to draw
+  ; if its 01, its a metasprite operation
+  ; if its 02, its a tile operation
+  ; we'll need to clear these out once we've fetched them 
 
-drawLoopSetup:
+  ;1 - check the stack pointer
+  ;1 -  if 0, nothing to do \ if not, we have to draw things
+  ;3 -  
+
+TestDraw:
+  lda drawBufferPointer
+  cmp #$00
+  bne PrepareVars
+  jmp EndDrawLogic
+
+PrepareVars:
+
+  ;op tile high low
+  ;***instead of poping here, we could do it in the game loop first
+  ; by passing to the stack, then passing to our buffer
+  ;that way we save more nmi time
+  
+  POP_DRAWING_BUFFER
+  sta draw_loop_start_low_byte
+  POP_DRAWING_BUFFER
+  sta draw_loop_start_high_byte
+  POP_DRAWING_BUFFER
+  sta draw_loop_sprite_low_byte
+  POP_DRAWING_BUFFER
+  sta draw_loop_sprite_high_byte
+  cmp #$00                      ;00 = no high byte = tile
+  bne DrawingMetaSpriteOperation
+
+DrawingTileOperation:
+  
+  ldx $2002                                 ; read PPU status to reset the high/low latch
+  ldx draw_loop_start_high_byte
+  stx $2006                                 ; write the high db of $2000 address
+  ldx draw_loop_start_low_byte              ; low byte var is first
+  stx $2006                                 ; write the low db of $2000 address
+  ldx draw_loop_sprite_low_byte                      ; tile hex
+  stx $2007
+  
+  jmp EndDrawLogic
+  
+;setup vars from stack
+DrawingMetaSpriteOperation:
+
+  ldy draw_loop_start_low_byte
+  lda (draw_loop_start_high_byte),Y
+
+  ; assigning extra relative variables for metasprite
+  ldx $2002                       ; read PPU status to reset the high/low latch
+  ldx draw_loop_start_high_byte
+  stx $2006                       ; write the high db of $2000 address
+  ldx draw_loop_start_low_byte    ; low byte var is first
+  LDY #$00
+  LDA draw_loop_sprite_high_byte, Y         ;sprite data at 00 (w)
+  STA draw_loop_width             ;store it in our var
+  LDY #$01
+  LDA draw_loop_sprite_high_byte, Y         ;sprite data at 01 (h)
+  STA draw_loop_height            ;store it in our var
   LDA #$00
-  STA do_draw
-  SET_METASPRITE_VARS metasprite, #$0D, #$21
+  STA draw_loop_height_counter
+  LDA #$00
+  STA draw_loop_current_x         ;this keeps track of our current x positon so we can check draw_loop_width
+                                  ;to know when to jump to the next row
+  ;we start to read the metasprite ahead 2 since the first two pos are w,h
+  LDY #$02
 
 drawLoopInit:
-  LDX $2002             ; read PPU status to reset the high/low latch
-  LDX draw_loop_start_high_byte
-  STX $2006             ; write the high db of $2000 address
-  LDA draw_loop_start_low_byte
-  STA $2006             ; write the low db of $2000 address
 
-drawLoop:
-  LDX metasprite, Y                 ; add tile on metasprite at Y in ppu
+  LDX $2002                         ; read PPU status to reset the high/low latch
+  LDX draw_loop_start_high_byte
+  STX $2006                         ; write the high db of $2000 address
+  LDA draw_loop_start_low_byte
+  STA $2006                         ; write the low db of $2000 address
+
+  drawLoop:
+  LDX  (draw_loop_sprite_high_byte), Y                 ; add tile on metasprite at Y in ppu
   STX $2007
   INY                               ;increment metasprite for tile
 
-  LDA draw_loop_current_x           ;icrement current position in row
+  LDA draw_loop_current_x           ;increment current position in row
   MACRO_ADD_A #$01
   STA draw_loop_current_x 
 
@@ -502,12 +638,12 @@ drawLoop:
   BCS increment_high_byte           ;then increment the high_byte
   JMP init_new_row
 
-increment_high_byte:
+  increment_high_byte:
   LDX draw_loop_start_high_byte
   INX
   STX draw_loop_start_high_byte
 
-init_new_row:
+  init_new_row:
   ;here, we check to see if the loop_height_counter has exceeded the loop_height
   LDA draw_loop_height_counter
   CMP draw_loop_height              ;compare current position of row with height of metasprites draw operation
@@ -515,10 +651,25 @@ init_new_row:
 
 EndDrawLogic:
 
-                ;clear out drawing request container
+  ;CLEAR_DRAWING_BUFFER
+
   RESET_SCROLL  ;reset the scroll
 
+  pla ;restoring our registers before NMI started
+  tax
+  pla
+  tay
+  pla
+
   RTI ;end of NMI/Done drawing
+
+;-----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 ;Vectors  
 
